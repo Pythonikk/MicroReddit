14702e1d95388ef7c803e4a3cc1c0c54d689bb3f
Pythonikk <nikkibrusoe@gmail.com>
2022-04-20 21:08:57 -0700

    initial commit
    -user model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  validates :pseudo, presence: true, length: { in: 5..20 }
+
+  validates :password, presence: true, length: { in: 6..20 }, format:
+    { with: /\A(?=.*[a-zA-Z])(?=.*[0-9]).{6,}\z/,
+      message: 'must have at least 6 characters and include one number and one letter' }
+end
